28e6d0c96739624c49c1d362526ee09522cc5b3f
Javier Ignacio Canales Castro <javier.canales1601@alumnos.ubiobio.cl>
2021-04-16 19:01:28 -0400

    Changed Port

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+const PORT = process.env.PORT || 5000;
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
@@ -18,6 +19,6 @@ app.options('*', cors());
 
 app.use('/api', routes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log("### Running at port 5000 ###");
 });
